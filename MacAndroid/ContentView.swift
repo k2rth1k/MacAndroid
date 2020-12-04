2599b4a9f23d723bfd4dd211407de0741a74307f
Nelapati karthik Chowdary <k.nelapati@f5.com>
2020-12-04 19:19:33 +0530

    Initial Commit

---
+++ MacAndroid/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  MacAndroid
+//
+//  Created by Nelapati Karthik CHOWDARY on 04/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
